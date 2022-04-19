9bb743195e7803e3602a3b31678dfb90360287af
romaindupont <rd.romaindupont@free.fr>
2022-04-19 21:55:47 +0200

    menu no overflow

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,4 @@
+const body = document.querySelector('body');
 const SubMenu = document.querySelectorAll('#menu-main-menu > .menu-item > a');
 const MainHeader = document.querySelector('.main-header');
 const SubMenuMark1 = document.querySelector('.mark1 > ul');
@@ -15,10 +16,13 @@ const closeMenu = document.querySelectorAll('.closeMenu');
 const OpenMenu = (e) => {
 	if(e.path[0].innerHTML === 'Mark 1') {
 		SubMenuMark1.style.display = SubMenuMark1.style.display === 'grid' ? 'none' : 'grid';
+		body.style.overflowY = 'hidden';
 		SubMenuMark1.style.display === 'grid' ? MainHeader.style.paddingBottom = "20px" : MainHeader.style.paddingBottom = "0px";
 		SubMenuMark1.style.display === 'grid' ? linkActiveMark1.classList.add('active') : linkActiveMark1.classList.remove('active');
 		SubMenuMark1.style.display === 'grid' ? linkActiveMark2.classList.remove('active') : linkActiveMark2.classList.remove('active');
 		SubMenuMark2.style.display = SubMenuMark2.style.display === 'none' ? '' : 'none';
+		SubMenuMark1.style.display === 'grid' ? body.style.overflowY = 'hidden' : body.style.overflowY = 'auto';
+
 		imageHelmetMenuMark1[0].src = image1;
 		submenuTitle[0].innerHTML= menuMark1;
 		closeMenu[0].addEventListener("click", ()=> {
@@ -26,14 +30,17 @@ const OpenMenu = (e) => {
 			MainHeader.style.paddingBottom = "0px";
 			linkActiveMark2.classList.remove('active');
 			linkActiveMark1.classList.remove('active');
+			body.style.overflowY = 'auto';
 		});
 	}
 	if(e.path[0].innerHTML === 'Mark 2') {
 		SubMenuMark2.style.display = SubMenuMark2.style.display === 'grid' ? 'none' : 'grid';
+		
 		SubMenuMark2.style.display === 'grid' ? MainHeader.style.paddingBottom = "20px" : MainHeader.style.paddingBottom = "0px";
 		SubMenuMark2.style.display === 'grid' ? linkActiveMark2.classList.add('active') : linkActiveMark2.classList.remove('active');
 		SubMenuMark2.style.display === 'grid' ? linkActiveMark1.classList.remove('active') : linkActiveMark1.classList.remove('active');
-	
+		SubMenuMark2.style.display === 'grid' ? body.style.overflowY = 'hidden' : body.style.overflowY = 'auto';
+
  		SubMenuMark1.style.display = SubMenuMark1.style.display === 'none' ? '' : 'none';
 		imageHelmetMenuMark1[1].src = image1;
 		submenuTitle[1].innerHTML= menuMark2;
@@ -42,6 +49,7 @@ const OpenMenu = (e) => {
 			MainHeader.style.paddingBottom = "0px";
 			linkActiveMark2.classList.remove('active');
 			linkActiveMark1.classList.remove('active');
+			body.style.overflowY = 'auto';
 		});
 	}
 
