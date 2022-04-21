bf06337579fe327d0c6f727f75bcc9fc4eca7df5
Romain DUPONT <RDUPONT@hydequip.lan>
2022-04-21 14:49:03 +0200

    essai propre tabs css + ajout down

--- footer-configurator.php
+++ footer-configurator.php
@@ -8,15 +8,7 @@
 	<div class="sd-tabs">
 		<input class="sd-tab-radio" name="tabs" tabindex="1" type="radio" id="tabone" checked="checked"/>
 		<label class="sd-tab-label label1" for="tabone">
-		<svg version="1.1" id="onglet" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-	 viewBox="0 0 300 45" style="enable-background:new 0 0 300 45;" xml:space="preserve">
-<path class="st0" d="M300,43h-30c-11,0-20-9-20-20l0,0c0-11-9-20-20-20H70c-11,0-20,9-20,20l0,0c0,11-9,20-20,20H0v2h300V43z"/>
-<path class="tab_on" id="on" d="M300,43h-30c-11,0-20-9-20-20l0,0c0-11-9-20-20-20H70c-11,0-20,9-20,20l0,0c0,11-9,20-20,20H0"/>
-<path class="st0" d="M264.3,43.1c-75.4,0.1-150.8,0.2-226.2,0.3v-1.3c74.5,0,149-0.1,223.6-0.1c0.3,0.2,0.7,0.4,1.1,0.5
-	C263.2,42.7,263.8,42.9,264.3,43.1z"/>
-</svg>
-
-			<div class="sd-tab-desc">Helmet</div>
+					<div class="sd-tab-desc">Helmet</div>
 			<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="eye" x="0px" y="0px" viewBox="0 0 31.2 31.2" xml:space="preserve">
 				<path class="eye-on" id="on" d="M15.6,0.6c-8.3,0-15,6.7-15,15s6.7,15,15,15s15-6.7,15-15S23.9,0.6,15.6,0.6z M15.6,20.4c-5,0-9.1-4.8-9.1-4.8  s1.5-1.7,3.7-3.1l2.1,2.1c-0.1,0.3-0.2,0.6-0.2,1c0,1.9,1.6,3.5,3.5,3.5c0.3,0,0.7-0.1,1-0.2l1.2,1.2C17.1,20.3,16.3,20.4,15.6,20.4  z M13.9,16.2l1,1C14.5,17.1,14.1,16.7,13.9,16.2z M21,21.8L9.4,10.1l0.8-0.8L21.8,21L21,21.8z M16.2,13.9c0.5,0.2,0.8,0.6,1,1  L16.2,13.9z M21,18.7l-2.1-2.1c0.1-0.3,0.2-0.6,0.2-1c0-1.9-1.6-3.5-3.5-3.5c-0.3,0-0.7,0.1-1,0.2l-1.2-1.2c0.7-0.2,1.4-0.3,2.2-0.3  c5,0,9.1,4.8,9.1,4.8S23.3,17.3,21,18.7z"></path>
 			</svg>
@@ -24,10 +16,7 @@
 		<div class="sd-tab-content" tabindex="1"></div>
 		<input class="sd-tab-radio" tabindex="1" name="tabs" type="radio" id="tabtwo"/>
 		<label class="sd-tab-label label2" for="tabtwo">
-		<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="onglet" x="0px" y="0px" viewBox="0 0 300 45" xml:space="preserve">
-			<path class="tab_off" id="off" d="M300,46c0,0-50,0-50,0V23c0-11-9-20-20-20L70,3c-11,0-20,9-20,20v20c0,0-50,0-50,0"/>
-		</svg>
-			<div class="sd-tab-desc">Chinguard</div>
+					<div class="sd-tab-desc">Chinguard</div>
 			<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="eye" x="0px" y="0px" viewBox="0 0 31.2 31.2" xml:space="preserve">
 				<g>
 					<path class="eye_off" id="off" d="M15.6,1.6c7.7,0,14,6.3,14,14s-6.3,14-14,14s-14-6.3-14-14S7.9,1.6,15.6,1.6 M15.6,0.6c-8.3,0-15,6.7-15,15   s6.7,15,15,15s15-6.7,15-15S23.9,0.6,15.6,0.6L15.6,0.6z"></path>
@@ -41,9 +30,6 @@
 		<div class="sd-tab-content" tabindex="1"></div>
 		<input class="sd-tab-radio" tabindex="1" name="tabs" type="radio" id="tabthree"/>
 		<label class="sd-tab-label label3" for="tabthree">
-			<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="onglet" x="0px" y="0px" viewBox="0 0 300 45" xml:space="preserve">
-				<path class="tab_off" id="off" d="M300,46c0,0-50,0-50,0V23c0-11-9-20-20-20L70,3c-11,0-20,9-20,20v20c0,0-50,0-50,0"/>
-			</svg>
 			<div class="sd-tab-desc">Visor</div>
 			<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="eye" x="0px" y="0px" viewBox="0 0 31.2 31.2" xml:space="preserve">
 				<g>
