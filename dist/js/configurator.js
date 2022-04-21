694a6ad81f2b579fb84e4b7088c733d5ce0d5377
Romain DUPONT <RDUPONT@hydequip.lan>
2022-04-21 16:11:53 +0200

    add exportButton optionButton eyesfx

--- dist/js/configurator.js
+++ dist/js/configurator.js
@@ -1,10 +1,10 @@
-
- const configurator = {
+const configurator = {
    init: function() {
     configurator.initElements();
 		configurator.OpenCloseMenu();
 		configurator.subMenuOpenClose();
 		configurator.tabsOpenClose();
+		configurator.clicOnEye();
     
   },
   initElements: function() {
@@ -19,7 +19,7 @@
 		configurator.labelThree = document.querySelector('.label3');
 		configurator.footerConfigurator = document.querySelector('.footer-configurator');
 		configurator.downButton = document.querySelector('#down');
-
+		configurator.eyes = document.querySelectorAll('#eye');
   },
 	OpenCloseMenu: function() {
     configurator.menu.addEventListener('click', configurator.handleMenuOpen);
@@ -35,10 +35,10 @@
   },
 	subMenuOpenClose: function() {
 		configurator.subMenu.forEach(element => {
-			element.addEventListener('click', configurator.handleSubMenuOpenClose)
+			element.addEventListener('click', configurator.handleSubMenuOpenClose);
 		});
 		configurator.closeSubMenu.forEach(element => {
-			element.addEventListener('click', configurator.handleSubMenuOpenClose)
+			element.addEventListener('click', configurator.handleSubMenuOpenClose);
 		});
   },
 	handleSubMenuOpenClose: function(e) {
@@ -68,6 +68,33 @@
   },
 	handleOpenCloseTabs: function() {
 		configurator.footerConfigurator.classList.toggle('openIt');
+	},
+	clicOnEye: function() {
+		configurator.eyes.forEach(eye => eye.addEventListener('click', 
+		() => {
+			if(eye.querySelector('#on')){
+				eye.innerHTML=`
+				<g>
+				<path class="eye_off" id="off" d="M15.6,1.6c7.7,0,14,6.3,14,14s-6.3,14-14,14s-14-6.3-14-14S7.9,1.6,15.6,1.6 M15.6,0.6c-8.3,0-15,6.7-15,15
+					s6.7,15,15,15s15-6.7,15-15S23.9,0.6,15.6,0.6L15.6,0.6z"/>
+			</g>
+			<g>
+				<circle class="eye_off" cx="15.6" cy="15.6" r="1.8"/>
+				<path class="eye_off" d="M15.6,10.8c-5,0-9.1,4.8-9.1,4.8s4.1,4.8,9.1,4.8c5,0,9.1-4.8,9.1-4.8S20.6,10.8,15.6,10.8z M15.6,19.1
+					c-1.9,0-3.5-1.6-3.5-3.5c0-1.9,1.6-3.5,3.5-3.5c1.9,0,3.5,1.6,3.5,3.5C19.1,17.5,17.5,19.1,15.6,19.1z"/>
+			</g>
+				 `}
+				 else {
+					eye.innerHTML=`
+					<path class="eye-on" id="on" d="M15.6,0.6c-8.3,0-15,6.7-15,15s6.7,15,15,15s15-6.7,15-15S23.9,0.6,15.6,0.6z M15.6,20.4c-5,0-9.1-4.8-9.1-4.8
+					s1.5-1.7,3.7-3.1l2.1,2.1c-0.1,0.3-0.2,0.6-0.2,1c0,1.9,1.6,3.5,3.5,3.5c0.3,0,0.7-0.1,1-0.2l1.2,1.2C17.1,20.3,16.3,20.4,15.6,20.4
+					z M13.9,16.2l1,1C14.5,17.1,14.1,16.7,13.9,16.2z M21,21.8L9.4,10.1l0.8-0.8L21.8,21L21,21.8z M16.2,13.9c0.5,0.2,0.8,0.6,1,1
+					L16.2,13.9z M21,18.7l-2.1-2.1c0.1-0.3,0.2-0.6,0.2-1c0-1.9-1.6-3.5-3.5-3.5c-0.3,0-0.7,0.1-1,0.2l-1.2-1.2c0.7-0.2,1.4-0.3,2.2-0.3
+					c5,0,9.1,4.8,9.1,4.8S23.3,17.3,21,18.7z"/>
+					 `
+				 }
+		}
+		));
 	},
 };
 
--- footer-configurator.php
+++ footer-configurator.php
@@ -1,15 +1,39 @@
 <footer class="footer-configurator">
-	<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="down" x="0px" y="0px" viewBox="0 0 41 41" xml:space="preserve">
+	<svg version="1.1" id="down" viewBox="0 0 41 41">
 		<polygon class="down" points="12,15.1 9.9,17.3 18.4,25.7 20.5,27.9 31.1,17.3 29,15.1 20.5,23.6 "></polygon>
 		<g>
 			<path class="down" d="M20.5,1.5c10.5,0,19,8.5,19,19s-8.5,19-19,19s-19-8.5-19-19S10,1.5,20.5,1.5 M20.5,0.5c-11,0-20,9-20,20   s9,20,20,20s20-9,20-20S31.5,0.5,20.5,0.5L20.5,0.5z"></path>
+		</g>
+	</svg>
+	<svg version="1.1" id="export" viewBox="0 0 35 35">
+		<style type="text/css">
+			.export{fill:#233658;}
+		</style>
+		<g>
+			<path class="export" d="M26.3,25.4l2.9-2.1v10.2H0V5.1h10.5L7.2,8H2.9v22.6h23.4V25.4L26.3,25.4z M8.1,21.6c-0.1,0.2-0.3,0.3-0.5,0.2   c-0.2,0-0.3-0.2-0.3-0.4c0.5-2.5,1.1-4.5,1.9-6.2c0.8-1.7,1.7-3.1,2.9-4.4s2.7-2.2,4.6-2.7c1.8-0.6,4.1-0.9,6.7-0.9V1.5L35,11.7   L23.4,21.8V16c-1.6,0-2.9,0-4,0.1c-1.1,0.1-2.4,0.3-3.8,0.7c-1.4,0.4-2.8,0.9-4,1.7C10.3,19.4,9.1,20.4,8.1,21.6z"></path>
+		</g>
+	</svg>
+	<svg version="1.1" id="menu_option" viewBox="0 0 41 41">
+		<style type="text/css">
+			.menuOption{fill:#233658;}
+		</style>
+		<g>
+			<path class="menuOption" d="M20.5,1.5c10.5,0,19,8.5,19,19s-8.5,19-19,19s-19-8.5-19-19S10,1.5,20.5,1.5 M20.5,0.5c-11,0-20,9-20,20   s9,20,20,20s20-9,20-20S31.5,0.5,20.5,0.5L20.5,0.5z"></path>
+		</g>
+		<g>
+			<path class="menuOption" d="M29.5,15.5h-13c-0.6,0-1-0.4-1-1v0c0-0.6,0.4-1,1-1h13c0.6,0,1,0.4,1,1v0C30.5,15.1,30.1,15.5,29.5,15.5z"></path>
+			<path class="menuOption" d="M29.5,21.5h-13c-0.6,0-1-0.4-1-1v0c0-0.6,0.4-1,1-1h13c0.6,0,1,0.4,1,1v0C30.5,21.1,30.1,21.5,29.5,21.5z"></path>
+			<path class="menuOption" d="M29.5,27.5h-13c-0.6,0-1-0.4-1-1v0c0-0.6,0.4-1,1-1h13c0.6,0,1,0.4,1,1v0C30.5,27.1,30.1,27.5,29.5,27.5z"></path>
+			<circle class="menuOption" cx="11.5" cy="14.5" r="1"></circle>
+			<circle class="menuOption" cx="11.5" cy="20.5" r="1"></circle>
+			<circle class="menuOption" cx="11.5" cy="26.5" r="1"></circle>
 		</g>
 	</svg>
 	<div class="sd-tabs">
 		<input class="sd-tab-radio" name="tabs" tabindex="1" type="radio" id="tabone" checked="checked"/>
 		<label class="sd-tab-label label1" for="tabone">
 					<div class="sd-tab-desc">Helmet</div>
-			<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="eye" x="0px" y="0px" viewBox="0 0 31.2 31.2" xml:space="preserve">
+			<svg version="1.1" id="eye" viewBox="0 0 31.2 31.2">
 				<path class="eye-on" id="on" d="M15.6,0.6c-8.3,0-15,6.7-15,15s6.7,15,15,15s15-6.7,15-15S23.9,0.6,15.6,0.6z M15.6,20.4c-5,0-9.1-4.8-9.1-4.8  s1.5-1.7,3.7-3.1l2.1,2.1c-0.1,0.3-0.2,0.6-0.2,1c0,1.9,1.6,3.5,3.5,3.5c0.3,0,0.7-0.1,1-0.2l1.2,1.2C17.1,20.3,16.3,20.4,15.6,20.4  z M13.9,16.2l1,1C14.5,17.1,14.1,16.7,13.9,16.2z M21,21.8L9.4,10.1l0.8-0.8L21.8,21L21,21.8z M16.2,13.9c0.5,0.2,0.8,0.6,1,1  L16.2,13.9z M21,18.7l-2.1-2.1c0.1-0.3,0.2-0.6,0.2-1c0-1.9-1.6-3.5-3.5-3.5c-0.3,0-0.7,0.1-1,0.2l-1.2-1.2c0.7-0.2,1.4-0.3,2.2-0.3  c5,0,9.1,4.8,9.1,4.8S23.3,17.3,21,18.7z"></path>
 			</svg>
 		</label>
@@ -17,7 +41,7 @@
 		<input class="sd-tab-radio" tabindex="1" name="tabs" type="radio" id="tabtwo"/>
 		<label class="sd-tab-label label2" for="tabtwo">
 					<div class="sd-tab-desc">Chinguard</div>
-			<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="eye" x="0px" y="0px" viewBox="0 0 31.2 31.2" xml:space="preserve">
+			<svg version="1.1" id="eye" viewBox="0 0 31.2 31.2">
 				<g>
 					<path class="eye_off" id="off" d="M15.6,1.6c7.7,0,14,6.3,14,14s-6.3,14-14,14s-14-6.3-14-14S7.9,1.6,15.6,1.6 M15.6,0.6c-8.3,0-15,6.7-15,15   s6.7,15,15,15s15-6.7,15-15S23.9,0.6,15.6,0.6L15.6,0.6z"></path>
 				</g>
@@ -31,7 +55,7 @@
 		<input class="sd-tab-radio" tabindex="1" name="tabs" type="radio" id="tabthree"/>
 		<label class="sd-tab-label label3" for="tabthree">
 			<div class="sd-tab-desc">Visor</div>
-			<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="eye" x="0px" y="0px" viewBox="0 0 31.2 31.2" xml:space="preserve">
+			<svg version="1.1" id="eye" viewBox="0 0 31.2 31.2">
 				<g>
 					<path class="eye_off" id="off" d="M15.6,1.6c7.7,0,14,6.3,14,14s-6.3,14-14,14s-14-6.3-14-14S7.9,1.6,15.6,1.6 M15.6,0.6c-8.3,0-15,6.7-15,15   s6.7,15,15,15s15-6.7,15-15S23.9,0.6,15.6,0.6L15.6,0.6z"></path>
 				</g>
